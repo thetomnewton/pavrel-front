d298aa3dc71ccb608147ef17c0afccb592eea38b
Tom Newton <newton.thomasjames@gmail.com>
2023-03-21 10:54:58 +0000

    init

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./components/**/*.{ts,vue,js}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
