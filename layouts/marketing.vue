51f352a4d9fb00b8a1ce7233f4a31b0fa434b7ca
Tom Newton <newton.thomasjames@gmail.com>
2023-03-21 11:25:07 +0000

    marketing/auth scaffold progress

--- layouts/marketing.vue
+++ layouts/marketing.vue
@@ -1,3 +1,25 @@
 <template>
-  <slot />
+  <header class="pt-4 px-8 max-w-4xl mx-auto">
+    <div class="flex items-center">
+      <div>logo</div>
+
+      <div class="ml-10 flex items-center space-x-6">
+        <a href="#features" class="font-medium text-sm text-slate-900 hover:text-slate-900">Features</a>
+        <a href="#pricing" class="font-medium text-sm text-slate-900 hover:text-slate-900">Pricing</a>
+      </div>
+
+      <div class="ml-auto">
+        <NuxtLink
+          to="/login"
+          class="inline-block rounded-lg px-3 py-1.5 text-sm font-medium leading-5 text-slate-800 hover:shadow-sm border border-slate-300 active:shadow-none active:translate-y-px transition-all"
+          >Log in</NuxtLink
+        >
+      </div>
+    </div>
+  </header>
+
+  <main>
+    <slot />
+  </main>
+  <footer></footer>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,32 @@
 definePageMeta({
   layout: 'marketing',
 })
+
+useHead({
+  title: 'Pavrel',
+  meta: [{ name: 'description', content: 'Unleash your creativity. Pavrel is a home for your ideas.' }],
+  bodyAttrs: {
+    class: 'bg-slate-50 antialiased font-sans',
+  },
+})
 </script>
 
 <template>
-  <h1 class="text-3xl font-semibold tracking-tight sm:text-5xl text-slate-900">Unleash your creativity.</h1>
-  <p class="mt-3 text-lg font-medium leading-8 text-slate-500">
-    Pavrel is a home for your ideas. Designed for modern software teams.
-  </p>
+  <section class="mx-auto max-w-4xl pt-20 pb-20 px-6 lg:px-8 sm:pt-24">
+    <h1 class="text-3xl font-semibold tracking-tight sm:text-5xl text-slate-900">Unleash your creativity.</h1>
+    <p class="mt-3 text-lg font-medium leading-8 text-slate-500">
+      Pavrel is a home for your ideas. Designed for modern software teams.
+    </p>
+
+    <div class="mt-8">
+      <NuxtLink
+        to="/register"
+        class="inline-block rounded-lg bg-gradient-to-b from-blue-600 to-blue-700 border border-blue-600 px-6 py-1.5 text-base font-semibold leading-7 outline-none text-white ring-0 focus:ring-2 focus:ring-offset-2 ring-blue-600 hover:bg-blue-700 hover:ring-blue-700 shadow-md transition-all active:from-blue-700 active:translate-y-px"
+      >
+        Get started free
+      </NuxtLink>
+    </div>
+
+    <div class="text-xs mt-3 text-slate-500">No credit card required.</div>
+  </section>
 </template>
