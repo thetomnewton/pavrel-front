e8524f8a30e862613b83e1151e55a98eabab82d4
Tom Newton <newton.thomasjames@gmail.com>
2023-03-21 11:04:34 +0000

    set up tailwind

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,6 @@
 <template>
-  <div class="font-bold text-red-500">hello world</div>
+  <h1 class="text-3xl font-semibold tracking-tight sm:text-5xl text-slate-900">Unleash your creativity.</h1>
+  <p class="mt-3 text-lg font-medium leading-8 text-slate-500">
+    Pavrel is a home for your ideas. Designed for modern software teams.
+  </p>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,39 @@
+const colors = require('tailwindcss/colors')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./components/**/*.{ts,vue,js}",
-    "./layouts/**/*.vue",
-    "./pages/**/*.vue",
-    "./plugins/**/*.{js,ts}",
-  ],
+  content: ['./components/**/*.{ts,vue,js}', './layouts/**/*.vue', './pages/**/*.vue', './plugins/**/*.{js,ts}'],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xs: '400px',
+      },
+
+      fontFamily: {
+        sans: ['Inter var, Inter, sans-serif', { fontFeatureSettings: '"cv11", "ss01"' }],
+      },
+
+      colors: {
+        slate: {
+          ...colors.slate,
+          ...{
+            150: '#e9edf6',
+          },
+        },
+      },
+
+      boxShadow: {
+        '3xl': 'rgb(0 0 0 / 50%) 0px 16px 70px',
+      },
+    },
   },
-  plugins: [],
-};
+
+  variants: {
+    extend: {
+      opacity: ['disabled'],
+    },
+  },
+
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography'), require('@headlessui/tailwindcss')],
+}
