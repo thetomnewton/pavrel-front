10d6a10263f68ac090b0aad5b05dc5a89f95d8ed
Tom Newton <newton.thomasjames@gmail.com>
2023-03-22 11:49:54 +0000

    Update broadcasting.ts

--- helpers/broadcasting.ts
+++ helpers/broadcasting.ts
@@ -3,35 +3,35 @@ import Pusher from 'pusher-js'
 import api from '../api'
 
 export function createEcho(backendUrl: string) {
-  if (typeof window !== 'undefined') {
-    // @ts-ignore
-    if (window.Pusher === undefined) window.Pusher = Pusher
+  if (typeof window === 'undefined') return
 
-    type AuthChannel = { name: string }
-    type AuthCallback = (error: boolean, data: object) => void
+  // @ts-ignore
+  if (window.Pusher === undefined) window.Pusher = Pusher
 
-    return new Echo({
-      broadcaster: 'pusher',
-      key: 'app-key',
-      wsHost: 'localhost',
-      wsPort: 6001,
-      forceTLS: backendUrl.split(':')[0] === 'https',
-      disableStats: true,
-      authorizer: (channel: AuthChannel) => ({
-        authorize: (socketId: number, callback: AuthCallback) => {
-          api
-            .post(`${backendUrl}/broadcasting/auth`, {
-              socket_id: socketId,
-              channel_name: channel.name,
-            })
-            .then(response => {
-              callback(false, response.data)
-            })
-            .catch(error => {
-              callback(true, error)
-            })
-        },
-      }),
-    })
-  }
+  type AuthChannel = { name: string }
+  type AuthCallback = (error: boolean, data: object) => void
+
+  return new Echo({
+    broadcaster: 'pusher',
+    key: 'app-key',
+    wsHost: 'localhost',
+    wsPort: 6001,
+    forceTLS: backendUrl.split(':')[0] === 'https',
+    disableStats: true,
+    authorizer: (channel: AuthChannel) => ({
+      authorize: (socketId: number, callback: AuthCallback) => {
+        api
+          .post(`${backendUrl}/broadcasting/auth`, {
+            socket_id: socketId,
+            channel_name: channel.name,
+          })
+          .then(response => {
+            callback(false, response.data)
+          })
+          .catch(error => {
+            callback(true, error)
+          })
+      },
+    }),
+  })
 }
