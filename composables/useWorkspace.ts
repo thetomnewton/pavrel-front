55b45f8daa84d5bfc00f4247904b07e72d377ae2
Tom Newton <newton.thomasjames@gmail.com>
2023-03-22 14:35:21 +0000

    fix

--- composables/useWorkspace.ts
+++ composables/useWorkspace.ts
@@ -39,10 +39,10 @@ export function useWorkspace() {
 
       Promise.allSettled(requests)
         .then(([userResp, workspacesResp]) => {
-          if (userResp.status === 'fulfilled') store.commit('base/setUser', userResp.value.data)
-          if (workspacesResp.status === 'fulfilled') store.commit('base/setWorkspaces', workspacesResp.value.data)
+          if (userResp?.status === 'fulfilled') store.commit('base/setUser', userResp.value.data)
+          if (workspacesResp?.status === 'fulfilled') store.commit('base/setWorkspaces', workspacesResp.value.data)
 
-          if (userResp.status !== 'fulfilled' || workspacesResp.status !== 'fulfilled') {
+          if (userResp?.status == 'rejected' || workspacesResp?.status === 'rejected') {
             reject('Workspace load error')
             return
           }
