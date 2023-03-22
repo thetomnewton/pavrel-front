e7018c10bf81f96317d704e565e582f7d2a3354e
Tom Newton <newton.thomasjames@gmail.com>
2023-03-22 09:16:34 +0000

    Update members.vue

--- pages/[workspaceSlug]/settings/members.vue
+++ pages/[workspaceSlug]/settings/members.vue
@@ -1,9 +1,59 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import dayjs from 'dayjs'
+
 definePageMeta({
   layout: 'settings',
 })
+
+const route = useRoute()
+const { getWorkspaceFromSlug } = useWorkspace()
+const { users } = useUsers()
+
+const workspace = getWorkspaceFromSlug(route.params.workspaceSlug as string)
+
+const modalOpen = ref(false)
+
+function niceDate(date: string) {
+  return dayjs(date).format('MMM DD YYYY')
+}
 </script>
 
 <template>
-  <div></div>
+  <div class="mx-auto max-w-3xl py-10 px-6 lg:px-8" v-if="workspace">
+    <div class="mb-8 border-b border-slate-200 pb-8 dark:border-zinc-700">
+      <div class="text-[22px] font-medium">Members</div>
+    </div>
+
+    <div>
+      <div class="mb-5 items-center md:flex">
+        <div class="mb-3 md:mb-0">
+          <div class="mb-[4px] text-[15px] font-medium">Workspace members</div>
+          <p class="text-[.8125rem] text-slate-600 dark:text-zinc-400">
+            Manage who has access to the {{ workspace.name }} workspace.
+          </p>
+        </div>
+
+        <span class="ml-auto">
+          <BaseButton button="primary" class="whitespace-nowrap" @click.native="modalOpen = true">
+            Invite people
+          </BaseButton>
+        </span>
+      </div>
+
+      <div>
+        <div v-for="user in users" :key="user.id" class="mb-4 flex items-center">
+          <img class="mr-3 h-6 w-6 rounded-full object-cover" :src="user.profile_photo_url" />
+
+          <span class="min-w-[35%] cursor-default text-[14px] font-medium">{{ user.name }}</span>
+          <span class="ml-2 cursor-default text-[.8125rem] text-slate-600 dark:text-zinc-400">{{ user.email }}</span>
+          <span class="ml-auto text-[.8125rem] text-slate-600 dark:text-zinc-400"
+            >Since {{ niceDate(user.created_at) }}</span
+          >
+        </div>
+      </div>
+    </div>
+
+    <WorkspaceInviteModal :open="modalOpen" @close="modalOpen = false" :workspace="workspace" />
+  </div>
 </template>
