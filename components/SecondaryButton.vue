eeba52a33c09e8840c8119ea1dca3f10fcb194f6
Tom Newton <newton.thomasjames@gmail.com>
2023-03-26 18:20:49 +0100

    Update SecondaryButton.vue

--- components/SecondaryButton.vue
+++ components/SecondaryButton.vue
@@ -12,7 +12,7 @@ export default {
 <template>
   <button
     :type="type"
-    class="inline-flex items-center rounded-md border border-slate-300 bg-white px-4 py-2 text-sm font-medium text-slate-700 shadow-sm transition hover:text-slate-500 focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500 active:bg-slate-50 active:text-slate-800 disabled:opacity-25 dark:border-zinc-700 dark:bg-zinc-800 dark:text-zinc-300 dark:hover:border-zinc-600 dark:hover:bg-zinc-800 dark:hover:text-zinc-300 dark:active:border-blue-500 dark:active:bg-zinc-800 dark:active:text-zinc-300"
+    class="inline-flex cursor-default items-center rounded-md border border-slate-300 bg-white px-4 py-2 text-sm font-medium text-slate-700 shadow-sm transition focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500 active:bg-slate-50 active:text-slate-800 disabled:opacity-25 dark:border-zinc-700 dark:bg-zinc-800 dark:text-zinc-300 dark:hover:border-zinc-600 dark:hover:bg-zinc-800 dark:hover:text-zinc-300 dark:active:border-blue-500 dark:active:bg-zinc-800 dark:active:text-zinc-300"
   >
     <slot></slot>
   </button>
