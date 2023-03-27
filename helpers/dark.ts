0a48a66dfb944897558fa7c5884a575192049ba4
Tom Newton <newton.thomasjames@gmail.com>
2023-03-27 22:19:49 +0100

    Update dark.ts

--- helpers/dark.ts
+++ helpers/dark.ts
@@ -5,16 +5,17 @@ export function watchForDarkMode() {
     const hasSystemDarkTheme =
       !('colorTheme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches
 
-    if (hasDarkThemeSetting || (usingSystemDarkSettings && hasSystemDarkTheme))
-      document.documentElement.classList.add('dark')
-    else document.documentElement.classList.remove('dark')
+    const doc = document.documentElement
+
+    if (hasDarkThemeSetting || (usingSystemDarkSettings && hasSystemDarkTheme)) doc.classList.add('dark')
+    else doc.classList.remove('dark')
 
     // If using system colours and the dark mode settings change, update the theme accordingly.
     window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
       if (!usingSystemDarkSettings) return
 
-      if (e.matches) document.documentElement.classList.add('dark')
-      else document.documentElement.classList.remove('dark')
+      if (e.matches) doc.classList.add('dark')
+      else doc.classList.remove('dark')
     })
   } catch (_) {}
 }
