1ec66db8adf707840d34377bcc0ec6b54493965a
Tom Newton <newton.thomasjames@gmail.com>
2023-03-27 20:37:34 +0100

    add icon

--- pages/[workspaceSlug]/inbox.vue
+++ pages/[workspaceSlug]/inbox.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { InboxIcon } from '@heroicons/vue/24/outline'
+
 definePageMeta({
   middleware: 'auth',
 })
@@ -35,10 +37,27 @@ const {
           :selected="isSelectedNotification(notification)"
           @click="selectInboxItem(notification)"
         />
+
+        <template v-if="!notifications.length">
+          <div class="flex flex-col items-center justify-center pt-24">
+            <div
+              class="rounded-full border border-slate-100 bg-slate-50 p-[10px] text-slate-500 dark:border-zinc-800 dark:bg-zinc-800 dark:text-zinc-400"
+            >
+              <InboxIcon class="h-6 w-6" />
+            </div>
+            <div class="mt-2 text-xs font-medium text-slate-500 dark:text-zinc-400">No items</div>
+          </div>
+        </template>
       </div>
     </div>
 
     <div class="flex-1">
+      <div v-if="!notifications.length" class="flex h-full items-center justify-center p-12">
+        <div class="flex flex-col items-center justify-center">
+          <InboxIcon class="h-8 w-8 text-slate-400 dark:text-zinc-500" />
+        </div>
+      </div>
+
       <NotificationPreviewPane
         v-if="selectedIdea && selectedNotification"
         :notification="selectedNotification"
