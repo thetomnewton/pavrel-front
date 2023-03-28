c51eebb966bbc046cd1448b03b46e9147e88a50b
Tom Newton <newton.thomasjames@gmail.com>
2023-03-28 11:25:04 +0100

    go back to client-side validation

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
 import { watchForDarkMode } from '~/helpers/dark'
 import { Workspace } from '~~/types'
-import { Ref } from 'vue'
+import axios from '../api'
 
-const { api } = useApi()
 const router = useRouter()
 
 useHead({
@@ -36,18 +35,20 @@ useHead({
   ],
 })
 
-const { data: workspaces } = (await api('/workspaces')) as { data: Ref<Workspace[]> }
+if (process.client) {
+  const { data: workspaces } = await axios.get('/workspaces')
 
-// If we are logged in
-if (workspaces.value) {
-  // If we have a recent workspace in storage, go there
-  const storageLastWorkspace = process.client && localStorage.getItem('last-workspace')
+  // If we are logged in
+  if (workspaces) {
+    // If we have a recent workspace in storage, go there
+    const storageLastWorkspace = process.client && localStorage.getItem('last-workspace')
 
-  if (storageLastWorkspace) router.push(`/${storageLastWorkspace}/drafts`)
-  else {
-    // Go to the most recent workspace in the list. If none exists, go to onboarding
-    if (workspaces.value.length > 0) router.push(`/${(workspaces.value.at(-1) as Workspace).slug}/drafts`)
-    else router.push(`/welcome`)
+    if (storageLastWorkspace) router.push(`/${storageLastWorkspace}/drafts`)
+    else {
+      // Go to the most recent workspace in the list. If none exists, go to onboarding
+      if (workspaces.value.length > 0) router.push(`/${(workspaces.value.at(-1) as Workspace).slug}/drafts`)
+      else router.push(`/welcome`)
+    }
   }
 }
 
