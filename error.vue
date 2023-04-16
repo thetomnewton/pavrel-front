1ed2f5cf0ad30a0e8f01104c4c85d2ae49673b78
Tom Newton <newton.thomasjames@gmail.com>
2023-04-16 16:36:53 +0100

    Custom error page wip

---
+++ error.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+defineProps<{
+  error: { statusCode: number }
+}>()
+</script>
+
+<template>
+  <div v-if="error.statusCode === 401">
+    <div>Unauthorized</div>
+  </div>
+
+  <div v-else-if="error.statusCode === 403">
+    <div>Forbidden</div>
+  </div>
+
+  <div v-else-if="error.statusCode === 404">
+    <div>Page not found</div>
+  </div>
+
+  <div v-else-if="error.statusCode >= 500">
+    <div>Something went wrong</div>
+  </div>
+</template>
