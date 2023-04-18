dd378eb19aaf00d8ad6471b64bacef579d79a36e
Tom Newton <newton.thomasjames@gmail.com>
2023-04-18 09:55:43 +0100

    Update [teamSlug].vue

--- pages/[workspaceSlug]/settings/teams/[teamSlug].vue
+++ pages/[workspaceSlug]/settings/teams/[teamSlug].vue
@@ -11,7 +11,7 @@ definePageMeta({
 })
 
 const { currentWorkspace } = useWorkspace()
-const { currentWorkspaceTeams, updateTeam, updateTeamUserRole } = useTeams()
+const { currentWorkspaceTeams, updateTeam, updateTeamUserRole, joinTeam, totalIdeasInTeam } = useTeams()
 const { user, getUserById } = useUsers()
 const route = useRoute()
 const router = useRouter()
@@ -74,8 +74,11 @@ function changeTeamMemberRole(id: User['id'], role: TeamUserRoles) {
   if (team.value) updateTeamUserRole({ teamId: team.value.id, userId: id, role })
 }
 
-const iAmTeamAdmin = computed(() => team.value?.users.find(({ id }) => id === user.value.id)?.pivot.role === 'admin')
+const iAmTeamAdmin = computed<boolean>(
+  () => team.value?.users.find(({ id }) => id === user.value.id)?.pivot.role === 'admin'
+)
 
+const isMemberOfTeam = computed<boolean>(() => !!team.value?.users.find(({ id }) => user.value.id === id))
 const addTeamMembersModalOpen = ref(false)
 
 watch(
@@ -94,185 +97,211 @@ watch(
       <h2 class="text-[22px] font-medium">Team Settings › {{ team.name }}</h2>
     </div>
 
-    <div class="mb-10 space-y-6 border-b border-slate-200 pb-10 dark:border-zinc-700 md:flex md:space-x-6 md:space-y-0">
-      <div class="md:w-1/3">
-        <h3 class="text-lg font-medium text-slate-900 dark:text-zinc-200">Team details</h3>
-        <p class="mt-1 text-sm text-slate-600 dark:text-zinc-400">Set the basic information for the team.</p>
-      </div>
+    <template v-if="isMemberOfTeam">
+      <div
+        class="mb-10 space-y-6 border-b border-slate-200 pb-10 dark:border-zinc-700 md:flex md:space-x-6 md:space-y-0"
+      >
+        <div class="md:w-1/3">
+          <h3 class="text-lg font-medium text-slate-900 dark:text-zinc-200">Team details</h3>
+          <p class="mt-1 text-sm text-slate-600 dark:text-zinc-400">Set the basic information for the team.</p>
+        </div>
 
-      <div class="md:w-2/3">
-        <form @submit.prevent="attemptTeamUpdate" class="max-w-[350px] space-y-6">
-          <div>
-            <FormLabel for="teamName">Team name</FormLabel>
-            <div className="mt-1">
-              <FormTextInput id="teamName" v-model="newTeam.name" required />
-            </div>
-          </div>
-
-          <div>
-            <FormLabel for="teamIdentifier">Team identifier</FormLabel>
-            <div className="mt-1 flex items-start">
-              <FormTextInput
-                id="teamIdentifier"
-                :model-value="newTeam.slug"
-                @update:model-value="newTeam ? (newTeam.slug = $event.toUpperCase()) : ''"
-                class="max-w-[75px]"
-                required
-              />
-
-              <div class="mt-0.5 ml-4 text-xs text-slate-500 dark:text-zinc-400">
-                Updating this may cause old idea links to become invalid.
+        <div class="md:w-2/3">
+          <form @submit.prevent="attemptTeamUpdate" class="max-w-[350px] space-y-6">
+            <div>
+              <FormLabel for="teamName">Team name</FormLabel>
+              <div className="mt-1">
+                <FormTextInput id="teamName" v-model="newTeam.name" required />
               </div>
             </div>
-          </div>
 
-          <div
-            class="mt-2 flex items-center rounded-md bg-red-100 px-4 py-2 text-sm font-medium leading-5 text-red-800"
-            v-if="slugAlreadyExists"
-          >
-            <InformationCircleIcon class="mr-2 h-5 w-5 stroke-[2px]" />
-            <span
-              >That identifier is already being used by <span class="font-semibold">{{ slugAlreadyExists.name }}</span
-              >.</span
-            >
-          </div>
-
-          <div>
-            <FormLabel>Team privacy</FormLabel>
-            <div className="mt-1">
-              <Listbox
-                :value="newTeam.privacy"
-                @update-value="updatePrivacy"
-                :options="[
-                  { text: 'Public', value: 'public', icon: UsersIcon },
-                  { text: 'Private', value: 'private', icon: LockClosedIcon },
-                ]"
-              ></Listbox>
-            </div>
-
-            <div class="mt-1.5 text-xs text-slate-500 dark:text-zinc-400">
-              <template v-if="newTeam.privacy === 'public'">
-                Anyone can view or join a public team at any time.
-              </template>
-
-              <template v-else-if="newTeam.privacy === 'private'">
-                Private teams cannot ve viewed or joined by others. New members can only be added by existing members.
-              </template>
-            </div>
-          </div>
-
-          <div>
-            <Button :disabled="!newTeam.name || !newTeam.slug || slugAlreadyExists || teamUpdateStatus === 'updating'">
-              Save
-            </Button>
-          </div>
-        </form>
-      </div>
-    </div>
-
-    <div class="mb-10 space-y-6 border-b border-slate-200 pb-10 dark:border-zinc-700 md:flex md:space-x-6 md:space-y-0">
-      <div class="md:w-1/3">
-        <h3 class="text-lg font-medium text-slate-900 dark:text-zinc-200">Members</h3>
-        <p class="mt-1 text-sm text-slate-600 dark:text-zinc-400">Set who has access to the team.</p>
-      </div>
-
-      <div class="md:w-2/3">
-        <div class="space-y-6">
-          <div
-            v-for="teamUser in team.users"
-            :key="teamUser.id"
-            class="w-full space-y-2 lg:flex lg:items-center lg:space-y-0"
-          >
-            <template v-if="getUserById(teamUser.id)">
-              <div class="lg:mr-4">
-                <img
-                  :src="getUserById(teamUser.id)?.profile_photo_url"
-                  class="block h-8 w-8 min-w-[2rem] rounded-full object-cover"
+            <div>
+              <FormLabel for="teamIdentifier">Team identifier</FormLabel>
+              <div className="mt-1 flex items-start">
+                <FormTextInput
+                  id="teamIdentifier"
+                  :model-value="newTeam.slug"
+                  @update:model-value="newTeam ? (newTeam.slug = $event.toUpperCase()) : ''"
+                  class="max-w-[75px]"
+                  required
                 />
-              </div>
 
-              <div>
-                <div class="truncate lg:max-w-[200px]">{{ getUserById(teamUser.id)?.name }}</div>
-                <div class="text-[13px] leading-5 text-slate-600 dark:text-zinc-400">
-                  {{ getUserById(teamUser.id)?.email }}
+                <div class="mt-0.5 ml-4 text-xs text-slate-500 dark:text-zinc-400">
+                  Updating this may cause old idea links to become invalid.
                 </div>
               </div>
+            </div>
 
-              <div class="lg:ml-auto lg:flex lg:items-center">
-                <div class="max-w-[350px]">
-                  <Listbox
-                    class="min-w-[150px]"
-                    :value="teamUser.pivot.role"
-                    :disabled="!iAmTeamAdmin"
-                    @update-value="changeTeamMemberRole(teamUser.id, $event)"
-                    :options="[
-                      { value: 'admin', text: 'Admin' },
-                      { value: 'member', text: 'Member' },
-                    ]"
+            <div
+              class="mt-2 flex items-center rounded-md bg-red-100 px-4 py-2 text-sm font-medium leading-5 text-red-800"
+              v-if="slugAlreadyExists"
+            >
+              <InformationCircleIcon class="mr-2 h-5 w-5 stroke-[2px]" />
+              <span
+                >That identifier is already being used by <span class="font-semibold">{{ slugAlreadyExists.name }}</span
+                >.</span
+              >
+            </div>
+
+            <div>
+              <FormLabel>Team privacy</FormLabel>
+              <div className="mt-1">
+                <Listbox
+                  :value="newTeam.privacy"
+                  @update-value="updatePrivacy"
+                  :options="[
+                    { text: 'Public', value: 'public', icon: UsersIcon },
+                    { text: 'Private', value: 'private', icon: LockClosedIcon },
+                  ]"
+                ></Listbox>
+              </div>
+
+              <div class="mt-1.5 text-xs text-slate-500 dark:text-zinc-400">
+                <template v-if="newTeam.privacy === 'public'">
+                  Anyone can view or join a public team at any time.
+                </template>
+
+                <template v-else-if="newTeam.privacy === 'private'">
+                  Private teams cannot ve viewed or joined by others. New members can only be added by existing members.
+                </template>
+              </div>
+            </div>
+
+            <div>
+              <Button
+                :disabled="!newTeam.name || !newTeam.slug || slugAlreadyExists || teamUpdateStatus === 'updating'"
+              >
+                Save
+              </Button>
+            </div>
+          </form>
+        </div>
+      </div>
+
+      <div
+        class="mb-10 space-y-6 border-b border-slate-200 pb-10 dark:border-zinc-700 md:flex md:space-x-6 md:space-y-0"
+      >
+        <div class="md:w-1/3">
+          <h3 class="text-lg font-medium text-slate-900 dark:text-zinc-200">Members</h3>
+          <p class="mt-1 text-sm text-slate-600 dark:text-zinc-400">Set who has access to the team.</p>
+        </div>
+
+        <div class="md:w-2/3">
+          <div class="space-y-6">
+            <div
+              v-for="teamUser in team.users"
+              :key="teamUser.id"
+              class="w-full space-y-2 lg:flex lg:items-center lg:space-y-0"
+            >
+              <template v-if="getUserById(teamUser.id)">
+                <div class="lg:mr-4">
+                  <img
+                    :src="getUserById(teamUser.id)?.profile_photo_url"
+                    class="block h-8 w-8 min-w-[2rem] rounded-full object-cover"
                   />
                 </div>
 
-                <div class="mt-2 min-w-[70px] lg:ml-8 lg:mt-0">
-                  <template v-if="user.id === teamUser.id">
-                    <button
-                      type="button"
-                      class="appearance-none text-sm font-medium text-red-500 hover:underline"
-                      @click="openLeaveTeamModal"
-                    >
-                      Leave
-                    </button>
-                  </template>
-
-                  <template v-else>
-                    <button
-                      type="button"
-                      v-if="iAmTeamAdmin"
-                      class="appearance-none text-sm font-medium text-red-500 hover:underline"
-                      @click="openRemoveFromTeamModal(teamUser.id)"
-                    >
-                      Remove
-                    </button>
-                  </template>
+                <div>
+                  <div class="truncate lg:max-w-[200px]">{{ getUserById(teamUser.id)?.name }}</div>
+                  <div class="text-[13px] leading-5 text-slate-600 dark:text-zinc-400">
+                    {{ getUserById(teamUser.id)?.email }}
+                  </div>
                 </div>
-              </div>
-            </template>
-          </div>
 
-          <div class="mt-4">
-            <button
-              type="button"
-              @click="addTeamMembersModalOpen = true"
-              class="inline-flex cursor-default items-center rounded-md border border-slate-300 px-3 py-1.5 text-sm font-medium leading-5 text-slate-800 shadow-sm active:translate-y-px active:border-slate-400 active:shadow-none dark:border-zinc-600 dark:text-zinc-100"
-            >
-              <PlusIcon class="mr-1 -ml-1 h-5 w-5 text-slate-500 dark:text-zinc-400" />
-              <span>Add members</span>
-            </button>
+                <div class="lg:ml-auto lg:flex lg:items-center">
+                  <div class="max-w-[350px]">
+                    <Listbox
+                      class="min-w-[150px]"
+                      :value="teamUser.pivot.role"
+                      :disabled="!iAmTeamAdmin"
+                      @update-value="changeTeamMemberRole(teamUser.id, $event)"
+                      :options="[
+                        { value: 'admin', text: 'Admin' },
+                        { value: 'member', text: 'Member' },
+                      ]"
+                    />
+                  </div>
+
+                  <div class="mt-2 min-w-[70px] lg:ml-8 lg:mt-0">
+                    <template v-if="user.id === teamUser.id">
+                      <button
+                        type="button"
+                        class="appearance-none text-sm font-medium text-red-500 hover:underline"
+                        @click="openLeaveTeamModal"
+                      >
+                        Leave
+                      </button>
+                    </template>
+
+                    <template v-else>
+                      <button
+                        type="button"
+                        v-if="iAmTeamAdmin"
+                        class="appearance-none text-sm font-medium text-red-500 hover:underline"
+                        @click="openRemoveFromTeamModal(teamUser.id)"
+                      >
+                        Remove
+                      </button>
+                    </template>
+                  </div>
+                </div>
+              </template>
+            </div>
+
+            <div class="mt-4">
+              <button
+                type="button"
+                @click="addTeamMembersModalOpen = true"
+                class="inline-flex cursor-default items-center rounded-md border border-slate-300 px-3 py-1.5 text-sm font-medium leading-5 text-slate-800 shadow-sm active:translate-y-px active:border-slate-400 active:shadow-none dark:border-zinc-600 dark:text-zinc-100"
+              >
+                <PlusIcon class="mr-1 -ml-1 h-5 w-5 text-slate-500 dark:text-zinc-400" />
+                <span>Add members</span>
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
 
-    <LeaveTeamModal
-      v-if="team"
-      :team="team"
-      :open="leaveTeamModalOpen"
-      @close="leaveTeamModalOpen = false"
-      @leave="leaveTeamModalOpen = false"
-    />
+      <LeaveTeamModal
+        v-if="team"
+        :team="team"
+        :open="leaveTeamModalOpen"
+        @close="leaveTeamModalOpen = false"
+        @leave="leaveTeamModalOpen = false"
+      />
 
-    <RemoveFromTeamModal
-      v-if="team && userToRemoveFromTeam"
-      :team="team"
-      :user="userToRemoveFromTeam"
-      :open="removeFromTeamModalOpen"
-      @close="removeFromTeamModalOpen = false"
-    />
+      <RemoveFromTeamModal
+        v-if="team && userToRemoveFromTeam"
+        :team="team"
+        :user="userToRemoveFromTeam"
+        :open="removeFromTeamModalOpen"
+        @close="removeFromTeamModalOpen = false"
+      />
 
-    <AddTeamMembersModal
-      v-if="team"
-      :team="team"
-      :open="addTeamMembersModalOpen"
-      @close="addTeamMembersModalOpen = false"
-    />
+      <AddTeamMembersModal
+        v-if="team"
+        :team="team"
+        :open="addTeamMembersModalOpen"
+        @close="addTeamMembersModalOpen = false"
+      />
+    </template>
+
+    <template v-else>
+      <div class="mx-auto mt-12 max-w-lg rounded-lg border py-8 text-center shadow">
+        <div class="mb-3">
+          You are not yet a member of <span class="font-medium">{{ team.name }}</span
+          >.
+        </div>
+
+        <div class="mb-4 flex items-center justify-center space-x-3 text-sm">
+          <div>{{ team.users.length }} member{{ team.users.length === 1 ? '' : 's' }}</div>
+          <div>{{ totalIdeasInTeam(team) }} ideas</div>
+        </div>
+
+        <div>
+          <Button @click="team ? joinTeam(team.id) : ''"> Join team </Button>
+        </div>
+      </div>
+    </template>
   </div>
 </template>
