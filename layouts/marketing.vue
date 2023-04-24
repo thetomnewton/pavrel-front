d63790513eb1284fa1630328dbce3fd1d879c33c
Tom Newton <newton.thomasjames@gmail.com>
2023-04-24 10:18:52 +0100

    Update marketing.vue

--- layouts/marketing.vue
+++ layouts/marketing.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { Bars3Icon, XMarkIcon } from '@heroicons/vue/24/outline'
+
 useHead({
   bodyAttrs: { class: 'antialiased h-screen text-slate-900 dark:text-zinc-200' },
   title: 'Pavrel',
@@ -29,6 +31,8 @@ useHead({
     },
   ],
 })
+
+const mobileMenuOpen = ref(false)
 </script>
 
 <template>
@@ -39,19 +43,57 @@ useHead({
         <LogoColorText class="h-[30px] w-[96px] text-slate-800" />
       </NuxtLink>
 
-      <div class="ml-10 flex items-center space-x-6">
+      <div class="ml-10 hidden items-center space-x-6 sm:flex">
         <NuxtLink href="/#features" class="text-sm font-medium text-slate-900 hover:text-slate-900">Features</NuxtLink>
         <NuxtLink href="/#pricing" class="text-sm font-medium text-slate-900 hover:text-slate-900">Pricing</NuxtLink>
         <NuxtLink href="/changelog" class="text-sm font-medium text-slate-900 hover:text-slate-900">Changelog</NuxtLink>
       </div>
 
-      <div class="ml-auto">
+      <div class="relative ml-auto flex items-center space-x-2">
         <NuxtLink
           to="/login"
           class="inline-block whitespace-nowrap rounded-lg border border-slate-300 px-3 py-1.5 text-sm font-medium leading-5 text-slate-800 transition-all hover:shadow-sm active:translate-y-px active:shadow-none"
         >
           Log in
         </NuxtLink>
+
+        <div class="flex sm:hidden">
+          <button type="button" class="rounded-md py-2 px-3" @click="mobileMenuOpen = true">
+            <Bars3Icon class="h-6 w-6" />
+          </button>
+        </div>
+
+        <div
+          v-if="mobileMenuOpen"
+          class="absolute top-0 right-0 min-w-[250px] rounded-lg border border-slate-200 bg-white pb-1.5 shadow-md sm:hidden"
+        >
+          <div class="text-right">
+            <button type="button" class="p-2" @click="mobileMenuOpen = false">
+              <XMarkIcon class="h-6 w-6" />
+            </button>
+          </div>
+
+          <div class="-mt-2">
+            <NuxtLink
+              @click="mobileMenuOpen = false"
+              href="/#features"
+              class="block w-full px-3 py-1.5 text-sm font-medium text-slate-900 hover:bg-slate-100 hover:text-slate-900"
+              >Features</NuxtLink
+            >
+            <NuxtLink
+              @click="mobileMenuOpen = false"
+              href="/#pricing"
+              class="block w-full px-3 py-1.5 text-sm font-medium text-slate-900 hover:bg-slate-100 hover:text-slate-900"
+              >Pricing</NuxtLink
+            >
+            <NuxtLink
+              @click="mobileMenuOpen = false"
+              href="/changelog"
+              class="block w-full px-3 py-1.5 text-sm font-medium text-slate-900 hover:bg-slate-100 hover:text-slate-900"
+              >Changelog</NuxtLink
+            >
+          </div>
+        </div>
       </div>
     </div>
   </header>
