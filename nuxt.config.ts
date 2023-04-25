21c08628590a335238c8c363acb58adfb3941972
Tom Newton <newton.thomasjames@gmail.com>
2023-04-25 08:02:01 +0100

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,4 +10,9 @@ export default defineNuxtConfig({
     public: { backendUrl: '', websocketHost: '' },
   },
   css: ['~/assets/css/tailwind.css'],
+  nitro: {
+    prerender: {
+      routes: ['/', '/changelog'],
+    },
+  },
 })
