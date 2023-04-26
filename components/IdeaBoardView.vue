10246f97aac3e7974b5063ebbc09397c2444da36
Tom Newton <newton.thomasjames@gmail.com>
2023-04-26 09:48:19 +0100

    Update IdeaBoardView.vue

--- components/IdeaBoardView.vue
+++ components/IdeaBoardView.vue
@@ -2,11 +2,17 @@
 import { cloneDeep, groupBy } from 'lodash-es'
 import { Idea, IdeaStatus, Team } from '~/types'
 import { ideaStatusSort } from '~/helpers/ideas'
+import { PlusIcon } from '@heroicons/vue/20/solid'
+import { useStore } from 'vuex'
 
 const props = defineProps<{
   ideas: Idea[]
   team: Team
 }>()
+
+const store = useStore()
+
+const toggleQuickCreateIdeaModal = () => store.commit('base/toggleQuickCreateIdeaModal')
 
 const ideasGroupedByStatus = computed(() => groupBy(props.ideas, 'status_id'))
 
@@ -68,6 +74,15 @@ function nextIdea() {
         <div class="mb-6 flex items-center text-[13px] font-medium">
           <StatusIcon :category="status.category" class="mr-2" />
           <span>{{ status.name }}</span>
+
+          <div class="ml-auto">
+            <button
+              type="button"
+              class="cursor-default rounded p-0.5 text-slate-700 hover:bg-slate-100 active:bg-slate-150 dark:text-zinc-400 dark:hover:bg-zinc-800 dark:active:text-zinc-300"
+            >
+              <PlusIcon class="h-[18px] w-[18px]" @click="toggleQuickCreateIdeaModal" />
+            </button>
+          </div>
         </div>
 
         <div class="space-y-4">
