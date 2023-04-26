23f5f058dcb5c0b07de11d92625927138998190b
Tom Newton <newton.thomasjames@gmail.com>
2023-04-26 09:39:17 +0100

    extract component

---
+++ components/IdeaBoardItem.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+import { truncate } from 'lodash-es'
+import { Idea, Label, Team } from '~~/types'
+
+const props = defineProps<{
+  team: Team
+  idea: Idea
+}>()
+
+const { isIdeaUpvoted, ideaUpvoteCount, toggleUpvoteIdea } = useIdeaUpvotes()
+
+function ideaLabels(ids: Label[]): Label[] {
+  const isLabel = (label: Label | undefined): label is Label => !!label
+
+  return ids.map(({ id }) => props.team.labels.find(label => label.id === id)).filter(isLabel)
+}
+</script>
+
+<template>
+  <div
+    class="cursor-default rounded-md border border-slate-300 bg-slate-50 py-2 px-3 shadow-sm dark:border-zinc-600 dark:bg-zinc-800"
+  >
+    <div class="flex items-start">
+      <div class="text-[13px] font-medium text-slate-800 dark:text-zinc-300">
+        {{ truncate(idea.title, { length: 60 }) }}
+      </div>
+      <div class="ml-auto whitespace-nowrap pt-0.5 pl-2 text-xs text-slate-500 dark:text-zinc-400">
+        {{ team.slug }}-{{ idea.team_idea_id }}
+      </div>
+    </div>
+
+    <div class="mt-2 flex items-center text-[13px]">
+      <template v-if="idea.labels.length <= 2">
+        <IdeaLabel v-for="{ name, color } in ideaLabels(idea.labels)" :color="color" class="-ml-1.5 truncate">
+          {{ name }}
+        </IdeaLabel>
+      </template>
+
+      <template v-else>
+        <div class="flex items-center px-1">
+          <span class="inline-flex items-center">
+            <span
+              v-for="{ color } in ideaLabels(idea.labels)"
+              :style="{ backgroundColor: color }"
+              class="mr-1 h-[6px] w-[6px] rounded-full"
+            ></span>
+            <span class="ml-[6px] text-slate-800 dark:text-zinc-400">{{ idea.labels.length }} Labels</span>
+          </span>
+        </div>
+      </template>
+    </div>
+
+    <div class="mt-1 flex justify-end">
+      <IdeaUpvoter
+        :count="ideaUpvoteCount(idea)"
+        :is-upvoted="isIdeaUpvoted(idea)"
+        @click.stop="toggleUpvoteIdea(idea)"
+        class="ml-auto xs:ml-1"
+      />
+    </div>
+  </div>
+</template>
