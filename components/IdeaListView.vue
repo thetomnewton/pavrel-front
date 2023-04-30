6330508323147b6fabe7f61d84bb9a1f67502011
Tom Newton <newton.thomasjames@gmail.com>
2023-04-30 12:58:55 +0100

    move logic out to wrapper component

--- components/IdeaListView.vue
+++ components/IdeaListView.vue
@@ -1,150 +1,30 @@
 <script setup lang="ts">
-import { ref, computed, watch, onMounted, onUnmounted } from 'vue'
-import { useRouter } from 'vue-router'
+import { ref, computed } from 'vue'
 import { groupBy } from 'lodash-es'
-import { ideaStatusSort } from '../helpers/ideas'
-import IdeasList from './IdeasList.vue'
-import IdeaPreview from './IdeaPreview.vue'
-import IdeasEmptyState from './IdeasEmptyState.vue'
-import StatusIcon from './StatusIcon.vue'
-import IdeaActionsToast from './IdeaActionsToast.vue'
-import { Idea, IdeaStatus, PossibleCategoryFilters, Team } from '../types'
+import { ideaStatusSort } from '@/helpers/ideas'
+import { Idea, IdeaStatus, PossibleCategoryFilters, Team } from '@/types'
 import { useStore } from 'vuex'
-import IdeaActionsModal from './IdeaActionsModal.vue'
-import BulkDeleteIdeasModal from './BulkDeleteIdeasModal.vue'
 
 const store = useStore()
 
 const props = defineProps<{
   ideas: Idea[]
   categoryFilter: PossibleCategoryFilters | undefined
-  teamSlug: string
+  team: Team
+  checkedIdeaIds: Idea['id'][]
 }>()
 
-watch(
-  () => props.categoryFilter,
-  () => {
-    checkedIdeaIds.value = []
-  }
-)
-
-function checkAllIdeas() {
-  checkedIdeaIds.value = [...new Set(props.ideas.map(idea => idea.id))]
-}
-
-function uncheckAllIdeas() {
-  checkedIdeaIds.value = []
-}
-
-const quickCreateOpen = computed(() => store.state.base.quickCreateIdeaModalOpen)
-
-const selectAllListener = (e: KeyboardEvent) => {
-  if (quickCreateOpen.value || previewModalOpen.value) return
-
-  if (e.key.toLowerCase() === 'a' && (e.ctrlKey || e.metaKey)) {
-    e.preventDefault()
-    e.stopPropagation()
-    checkAllIdeas()
-  }
-}
-
-const escapeListener = (e: KeyboardEvent) => {
-  if (quickCreateOpen.value || previewModalOpen.value) return
-
-  if (e.key.toLowerCase() === 'escape') uncheckAllIdeas()
-}
-
-onMounted(() => {
-  document.addEventListener('keydown', selectAllListener)
-  document.addEventListener('keydown', escapeListener)
-})
-
-onUnmounted(() => {
-  document.removeEventListener('keydown', selectAllListener)
-  document.removeEventListener('keydown', escapeListener)
-})
-
-const router = useRouter()
-const { currentWorkspaceTeams } = useWorkspace()
-
-const previewModalOpen = ref(false)
-const selectedIdea = ref<string | null>(null)
-
-const team = computed<Team | undefined>(() =>
-  currentWorkspaceTeams.value?.find(({ slug }: Team) => slug === props.teamSlug)
-)
-
-const checkedIdeaIds = ref<Idea['id'][]>([])
+const emit = defineEmits(['select-idea', 'toggle-checked', 'uncheck-all'])
 
 const ideasGroupedByStatus = computed(() => groupBy(props.ideas, 'status_id'))
 
 const ideaStatusesSortedByCategory = computed(() => {
-  if (team.value == null) return []
+  if (props.team == null) return []
 
-  return team.value.statuses
+  return props.team.statuses
     .sort((a: IdeaStatus, b: IdeaStatus) => ideaStatusSort(a.category, b.category))
     .map(({ id }) => id)
 })
-
-const ideasListFlattened = computed(() => {
-  if (!team.value) return []
-
-  return props.ideas.sort((a, b) => {
-    // Check the status category
-    const categoryA = team.value?.statuses.find(({ id }) => id === a.status_id)?.category || 0
-    const categoryB = team.value?.statuses.find(({ id }) => id === b.status_id)?.category || 0
-
-    if (categoryA !== categoryB) return ideaStatusSort(categoryA, categoryB)
-
-    return +b.id - +a.id
-  })
-})
-
-const selectedIdeaIndex = computed(() => {
-  if (!selectedIdea.value) return null
-
-  return ideasListFlattened.value.findIndex(({ id }) => id === selectedIdea.value)
-})
-
-const prevState = router.options.history.state
-
-if (typeof prevState?.back === 'string') {
-  const match = prevState.back?.match(/(.*)(\/ideas\/)(.*)(\/edit)/)
-
-  // If we have come here from the edit idea page without refreshing, re-open the preview modal for the idea
-  if (match && prevState.scroll === null) {
-    const team = currentWorkspaceTeams.value.find(({ slug }: { slug: string }) => slug === match[3].split('-')[0])
-    const idea = props.ideas.find(idea => idea.team_id === team?.id && idea.team_idea_id === +match[3].split('-')[1])
-
-    if (idea) selectIdea(idea)
-  }
-}
-
-function closeIdeaPreviewModal() {
-  previewModalOpen.value = false
-  selectedIdea.value = null
-}
-
-function selectIdea({ id }: Idea) {
-  selectedIdea.value = id
-  previewModalOpen.value = true
-  checkedIdeaIds.value = []
-}
-
-function prevIdea() {
-  if (selectedIdeaIndex.value == null || ideasListFlattened.value[selectedIdeaIndex.value - 1] == null) return
-  selectedIdea.value = ideasListFlattened.value[selectedIdeaIndex.value - 1].id
-}
-
-function nextIdea() {
-  if (selectedIdeaIndex.value == null || ideasListFlattened.value[selectedIdeaIndex.value + 1] == null) return
-  selectedIdea.value = ideasListFlattened.value[selectedIdeaIndex.value + 1].id
-}
-
-function toggleIdeaChecked({ idea, value }: { idea: Idea; value: boolean }) {
-  if (value) checkedIdeaIds.value.push(idea.id)
-  else checkedIdeaIds.value.splice(checkedIdeaIds.value.indexOf(idea.id), 1)
-}
 
 const ideaActionsModalOpen = ref(false)
 const bulkDeleteIdeasModalOpen = ref(false)
@@ -156,9 +36,9 @@ function showActionsModal() {
 }
 
 function confirmBulkDeleteIdeas() {
-  bulkDeleteIdeas(checkedIdeaIds.value)
+  bulkDeleteIdeas(props.checkedIdeaIds)
   bulkDeleteIdeasModalOpen.value = false
-  checkedIdeaIds.value = []
+  emit('uncheck-all')
 }
 </script>
 
@@ -169,8 +49,8 @@ function confirmBulkDeleteIdeas() {
         <div
           class="flex cursor-default items-center bg-slate-50 py-2 pl-[22px] pr-6 text-[.8125rem] font-medium leading-[14px] text-slate-700 dark:bg-zinc-700/50 dark:text-zinc-200 lg:pl-10"
         >
-          <StatusIcon :category="team?.statuses.find(({ id }) => id === statusId)?.category" class="mr-2" />
-          <span>{{ team?.statuses.find(({ id }) => id === statusId)?.name }}</span>
+          <StatusIcon :category="team.statuses.find(({ id }) => id === statusId)?.category" class="mr-2" />
+          <span>{{ team.statuses.find(({ id }) => id === statusId)?.name }}</span>
         </div>
 
         <IdeasList
@@ -178,26 +58,16 @@ function confirmBulkDeleteIdeas() {
           :ideas="ideasGroupedByStatus[statusId]"
           :team="team"
           :checked="checkedIdeaIds"
-          @select-idea="selectIdea($event)"
-          @toggle-checked="toggleIdeaChecked"
+          @select-idea="$emit('select-idea', $event)"
+          @toggle-checked="$emit('toggle-checked', $event)"
         />
       </div>
     </template>
-
-    <IdeaPreview
-      :open="previewModalOpen"
-      :idea-id="selectedIdea ?? undefined"
-      :idea-index="selectedIdeaIndex"
-      :total-ideas="ideasListFlattened.length"
-      @close="closeIdeaPreviewModal"
-      @prev-idea="prevIdea"
-      @next-idea="nextIdea"
-    />
   </section>
 
   <IdeasEmptyState v-else />
 
-  <IdeaActionsToast :idea-ids="checkedIdeaIds" @show-actions-modal="showActionsModal" @cancel="checkedIdeaIds = []" />
+  <IdeaActionsToast :idea-ids="checkedIdeaIds" @show-actions-modal="showActionsModal" @cancel="$emit('uncheck-all')" />
 
   <IdeaActionsModal
     :open="ideaActionsModalOpen"
--- components/IdeaViewWrapper.vue
+++ components/IdeaViewWrapper.vue
@@ -1,20 +1,154 @@
 <script setup lang="ts">
+import { useStore } from 'vuex'
 import { Idea, PossibleCategoryFilters, Team } from '~/types'
+import { ideaStatusSort } from '../helpers/ideas'
 
-defineProps<{
+const props = defineProps<{
   view: 'list' | 'board'
   ideas: Idea[]
   team: Team
   categoryFilter: PossibleCategoryFilters | undefined
 }>()
+
+const router = useRouter()
+const { currentWorkspaceTeams } = useWorkspace()
+
+const store = useStore()
+
+watch(
+  () => props.categoryFilter,
+  () => {
+    checkedIdeaIds.value = []
+  }
+)
+
+const quickCreateOpen = computed(() => store.state.base.quickCreateIdeaModalOpen)
+
+const checkedIdeaIds = ref<Idea['id'][]>([])
+
+const previewModalOpen = ref(false)
+
+const selectedIdea = ref<string | null>(null)
+
+const selectedIdeaIndex = computed(() => {
+  if (!selectedIdea.value) return null
+
+  return ideasListFlattened.value.findIndex(({ id }) => id === selectedIdea.value)
+})
+
+function checkAllIdeas() {
+  checkedIdeaIds.value = [...new Set(props.ideas.map(idea => idea.id))]
+}
+
+function uncheckAllIdeas() {
+  checkedIdeaIds.value = []
+}
+
+const selectAllListener = (e: KeyboardEvent) => {
+  if (quickCreateOpen.value || previewModalOpen.value) return
+
+  if (e.key.toLowerCase() === 'a' && (e.ctrlKey || e.metaKey)) {
+    e.preventDefault()
+    e.stopPropagation()
+    checkAllIdeas()
+  }
+}
+
+const escapeListener = (e: KeyboardEvent) => {
+  if (quickCreateOpen.value || previewModalOpen.value) return
+
+  if (e.key.toLowerCase() === 'escape') uncheckAllIdeas()
+}
+
+const ideasListFlattened = computed(() => {
+  if (!props.team) return []
+
+  return props.ideas.sort((a, b) => {
+    // Check the status category
+    const categoryA = props.team.statuses.find(({ id }) => id === a.status_id)?.category || 0
+    const categoryB = props.team.statuses.find(({ id }) => id === b.status_id)?.category || 0
+
+    if (categoryA !== categoryB) return ideaStatusSort(categoryA, categoryB)
+
+    return +b.id - +a.id
+  })
+})
+
+function toggleIdeaChecked({ idea, value }: { idea: Idea; value: boolean }) {
+  if (value) checkedIdeaIds.value.push(idea.id)
+  else checkedIdeaIds.value.splice(checkedIdeaIds.value.indexOf(idea.id), 1)
+}
+
+onMounted(() => {
+  document.addEventListener('keydown', selectAllListener)
+  document.addEventListener('keydown', escapeListener)
+})
+
+onUnmounted(() => {
+  document.removeEventListener('keydown', selectAllListener)
+  document.removeEventListener('keydown', escapeListener)
+})
+
+function prevIdea() {
+  if (selectedIdeaIndex.value == null || ideasListFlattened.value[selectedIdeaIndex.value - 1] == null) return
+  selectedIdea.value = ideasListFlattened.value[selectedIdeaIndex.value - 1].id
+}
+
+function nextIdea() {
+  if (selectedIdeaIndex.value == null || ideasListFlattened.value[selectedIdeaIndex.value + 1] == null) return
+  selectedIdea.value = ideasListFlattened.value[selectedIdeaIndex.value + 1].id
+}
+
+function closeIdeaPreviewModal() {
+  previewModalOpen.value = false
+  selectedIdea.value = null
+}
+
+function selectIdea({ id }: Idea) {
+  selectedIdea.value = id
+  previewModalOpen.value = true
+  checkedIdeaIds.value = []
+}
+
+const prevState = router.options.history.state
+
+if (typeof prevState?.back === 'string') {
+  const match = prevState.back?.match(/(.*)(\/ideas\/)(.*)(\/edit)/)
+
+  // If we have come here from the edit idea page without refreshing, re-open the preview modal for the idea
+  if (match && prevState.scroll === null) {
+    const team = currentWorkspaceTeams.value.find(({ slug }: { slug: string }) => slug === match[3].split('-')[0])
+    const idea = props.ideas.find(idea => idea.team_id === team?.id && idea.team_idea_id === +match[3].split('-')[1])
+
+    if (idea) selectIdea(idea)
+  }
+}
 </script>
 
 <template>
   <template v-if="view === 'list'">
-    <IdeaListView :ideas="ideas" :teamSlug="team.slug" :categoryFilter="categoryFilter" />
+    <IdeaListView
+      :ideas="ideas"
+      :team="team"
+      :categoryFilter="categoryFilter"
+      :checkedIdeaIds="checkedIdeaIds"
+      @select-idea="selectIdea"
+      @toggle-checked="toggleIdeaChecked"
+      @uncheck-all="uncheckAllIdeas"
+    />
   </template>
 
   <template v-else>
     <IdeaBoardView :ideas="ideas" :team="team" />
   </template>
+
+  <IdeaPreview
+    :open="previewModalOpen"
+    :idea-id="selectedIdea ?? undefined"
+    :idea-index="selectedIdeaIndex"
+    :total-ideas="ideasListFlattened.length"
+    @close="closeIdeaPreviewModal"
+    @prev-idea="prevIdea"
+    @next-idea="nextIdea"
+  />
 </template>
