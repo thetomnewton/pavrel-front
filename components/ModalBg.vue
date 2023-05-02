3bea6fa999b5c11033b839ffd9d5eb441f8cc5f5
Tom Newton <newton.thomasjames@gmail.com>
2023-05-02 12:13:52 +0100

    make modal bg darker in dark mode

--- components/ModalBg.vue
+++ components/ModalBg.vue
@@ -11,6 +11,6 @@ import { TransitionChild } from '@headlessui/vue'
     leave-from="opacity-100"
     leave-to="opacity-0"
   >
-    <div class="fixed inset-0 bg-slate-900/20 px-3 py-6 dark:bg-zinc-900/60" aria-hidden="true"></div>
+    <div class="fixed inset-0 bg-slate-900/20 px-3 py-6 dark:bg-zinc-900/70" aria-hidden="true"></div>
   </TransitionChild>
 </template>
