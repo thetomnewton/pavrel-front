ea2122df42bad596747a7229846066010b4873f0
Tom Newton <newton.thomasjames@gmail.com>
2023-05-02 12:09:34 +0100

    add content to help section

--- components/HelpSlideover.vue
+++ components/HelpSlideover.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import { XMarkIcon } from '@heroicons/vue/20/solid'
+import { ArrowTopRightOnSquareIcon, XMarkIcon } from '@heroicons/vue/20/solid'
+import { BookOpenIcon, InboxArrowDownIcon } from '@heroicons/vue/24/outline'
 
 defineProps<{
   open: boolean
@@ -10,15 +11,15 @@ const emit = defineEmits(['close'])
 
 <template>
   <div
-    class="fixed inset-0 z-40 flex justify-end p-6 transition lg:p-8"
+    class="fixed inset-0 z-40 flex justify-end transition"
     :class="{ 'bg-black/20 dark:bg-black/50': open, 'pointer-events-none opacity-0': !open }"
     @click.self="emit('close')"
   >
     <div
-      class="min-w-[350px] rounded-lg bg-white shadow-md transition dark:border dark:border-zinc-600 dark:bg-zinc-900 lg:min-w-[400px]"
+      class="min-w-[350px] bg-white shadow-md transition dark:border-l dark:border-zinc-600 dark:bg-zinc-900 lg:min-w-[400px]"
       :class="{ 'translate-x-[20px]': !open }"
     >
-      <h3 class="flex items-center px-8 pt-6 font-semibold">
+      <h3 class="mb-6 flex items-center px-8 pt-6 font-semibold">
         <span>Help & Support</span>
         <span class="ml-auto">
           <button
@@ -30,6 +31,34 @@ const emit = defineEmits(['close'])
           </button>
         </span>
       </h3>
+
+      <div class="space-y-4 px-8">
+        <NuxtLink
+          href="/docs"
+          target="_blank"
+          rel="noreferrer"
+          class="flex w-full cursor-pointer items-center rounded-md border border-slate-200 px-4 py-3 text-sm font-medium leading-6 text-slate-800 hover:border-slate-300 dark:border-zinc-500 dark:text-zinc-200 dark:hover:bg-zinc-800"
+        >
+          <BookOpenIcon class="mr-3 h-6 w-6" />
+          <span>View documentation</span>
+          <span class="ml-auto pl-2">
+            <ArrowTopRightOnSquareIcon class="h-5 w-5 text-slate-500 dark:text-zinc-400" />
+          </span>
+        </NuxtLink>
+
+        <a
+          href="mailto:support@pavrel.com"
+          target="_blank"
+          rel="noreferrer"
+          class="flex w-full cursor-pointer items-center rounded-md border border-slate-200 px-4 py-3 text-sm font-medium leading-6 text-slate-800 hover:border-slate-300 dark:border-zinc-500 dark:text-zinc-200 dark:hover:bg-zinc-800"
+        >
+          <InboxArrowDownIcon class="mr-3 h-6 w-6" />
+          <span>Email us</span>
+          <span class="ml-auto pl-2">
+            <ArrowTopRightOnSquareIcon class="h-5 w-5 text-slate-500 dark:text-zinc-400" />
+          </span>
+        </a>
+      </div>
     </div>
   </div>
 </template>
