339f8927deec1edfac2238856f954ee367b320b5
Tom Newton <newton.thomasjames@gmail.com>
2023-05-05 15:17:46 +0100

    --wip-- [skip ci]

--- composables/useCsrf.ts
+++ composables/useCsrf.ts
@@ -2,10 +2,22 @@ import { csrf } from '~/helpers/auth'
 
 export function useCsrf() {
   const csrfInterval = ref<ReturnType<typeof setInterval> | null>(null)
+  const lastCsrfPing = ref<number | null>(null)
+
+  function pingCsrf() {
+    // Do nothing if the CSRF cookie was pinged in the last 5 minutes
+    if (Date.now() - (lastCsrfPing.value ?? 0) < 1000 * 60 * 5) return
+
+    csrf()
+    lastCsrfPing.value = Date.now()
+  }
 
   onMounted(() => {
-    csrfInterval.value = setInterval(csrf, 1000 * 60 * 10)
-    window.addEventListener('focus', csrf)
+    // Ping the CSRF cookie randomly every 20 minutes
+    csrfInterval.value = setInterval(pingCsrf, 1000 * 60 * 20)
+
+    // Ping the CSRF cookie when the window is focused
+    window.addEventListener('focus', pingCsrf)
   })
 
   onUnmounted(() => {
