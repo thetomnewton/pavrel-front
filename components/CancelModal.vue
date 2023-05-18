87fa562cb4f7d44019539cdfd41c2cd9e71e6c43
Tom Newton <newton.thomasjames@gmail.com>
2023-05-18 21:18:57 +0100

    wip

--- components/CancelModal.vue
+++ components/CancelModal.vue
@@ -22,12 +22,14 @@ function cancel() {
           class="mx-auto w-full max-w-[550px] rounded-md bg-white shadow-3xl dark:border dark:border-zinc-700 dark:bg-zinc-900 md:mt-[12vh]"
         >
           <form @submit.prevent="cancel" class="px-8 py-6">
-            <p>Are you sure you wish to cancel your subscription?</p>
+            <p class="mb-4">Are you sure you wish to cancel your subscription?</p>
 
-            <p>You will lose access to all Pro features.</p>
+            <p class="mb-4">You will lose access to all Pro features.</p>
 
-            <p class="flex items-center justify-end">
-              <button>Close</button>
+            <p class="flex items-center justify-end space-x-2">
+              <button type="button" @click="$emit('close')" class="rounded-md px-4 py-2 text-sm font-medium leading-5">
+                Close
+              </button>
               <Button>Cancel subscription</Button>
             </p>
           </form>
