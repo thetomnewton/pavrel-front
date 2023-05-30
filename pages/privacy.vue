90ad25cd11b24798f426176e704c2bbf1bbbb855
Tom Newton <newton.thomasjames@gmail.com>
2023-05-30 15:49:03 +0100

    privacy

--- pages/privacy.vue
+++ pages/privacy.vue
@@ -13,6 +13,114 @@ useHead({
   <div class="prose prose-slate mx-auto min-h-[600px] max-w-4xl px-6 py-16 lg:px-8">
     <h1>Privacy Policy</h1>
 
-    <p>Coming soon</p>
+    <p>Last Updated: May 30, 2023</p>
+
+    <p>
+      This Privacy Policy explains how Pavrel Limited ("we" or "us") collects, uses, and discloses personal information
+      when you use our SaaS platform, Pavrel ("the Service"). By accessing or using the Service, you agree to the terms
+      of this Privacy Policy.
+    </p>
+
+    <p>1. Information We Collect</p>
+
+    <p>We may collect the following types of personal information when you use the Service:</p>
+
+    <ul>
+      <li>Name</li>
+      <li>Email address</li>
+      <li>Profile photo</li>
+      <li>Text and images entered into your workspace</li>
+    </ul>
+
+    <p>2. Use of Personal Information</p>
+
+    <p>
+      We collect personal information solely for the purpose of providing the Service to you. We may use the collected
+      information to:
+    </p>
+
+    <ul>
+      <li>Create and manage your user account</li>
+      <li>Enable communication and notifications related to the Service</li>
+      <li>Personalize and improve the user experience</li>
+      <li>Respond to user inquiries or support requests</li>
+      <li>Understand and analyze usage patterns and preferences</li>
+      <li>Enforce our Terms of Service</li>
+    </ul>
+
+    <p>3. Data Retention</p>
+
+    <p>
+      We retain personal information for as long as necessary to fulfill the purposes outlined in this Privacy Policy.
+      Unless you request deletion of your data, it will be retained indefinitely. However, upon your request for data
+      deletion, we will promptly remove your information from our systems.
+    </p>
+
+    <p>4. Cookies and Tracking Technologies</p>
+
+    <p>
+      We use essential cookies to facilitate your login session and identify whether you are using dark mode. These
+      cookies are necessary for the proper functioning of the Service.
+    </p>
+
+    <p>5. Third-Party Service Providers</p>
+
+    <p>We engage third-party service providers to assist in delivering the Service. These providers include:</p>
+
+    <ul>
+      <li>Stripe: We use Stripe as a payment processor to handle transactions securely.</li>
+      <li>
+        Sentry: We utilize Sentry for error and exception tracking, helping us identify and resolve any issues with the
+        Service. These service providers may have access to personal information solely for the purpose of providing
+        their services and are obligated not to disclose or use it for any other purpose.
+      </li>
+    </ul>
+
+    <p>6. Data Security Measures</p>
+
+    <p>
+      We implement reasonable security measures to protect personal information from unauthorized access, disclosure,
+      alteration, or destruction. These measures include logical access controls, encryption of sensitive data, and
+      regular penetration testing. We aim to address any critical security vulnerabilities within 30 days of their
+      identification.
+    </p>
+
+    <p>7. User Rights</p>
+
+    <p>
+      You have the right to access, update, and delete your personal information held by us. If you wish to exercise
+      these rights, please contact us at the email address provided in Section 14 of this Privacy Policy. We will make
+      reasonable efforts to respond to your request within 14 days.
+    </p>
+
+    <p>8. Disclosure of Personal Information</p>
+
+    <p>
+      We may disclose personal information to a third party for the sole purpose of maintaining and improving the
+      Service. We do not sell or share personal information with third parties for their own marketing purposes.
+    </p>
+
+    <p>9. Children's Privacy</p>
+
+    <p>
+      The Service is not intended for individuals under the age of 16. We do not knowingly collect personal information
+      from children under 16. If you are a parent or guardian and believe that your child has provided us with personal
+      information, please contact us, and we will take steps to delete such information from our systems.
+    </p>
+
+    <p>10. Changes to the Privacy Policy</p>
+
+    <p>
+      Any changes we make to this Privacy Policy will be posted in the changelog. We recommend periodically reviewing
+      this Privacy Policy to stay informed about our information practices.
+    </p>
+
+    <p>11. Contact Information</p>
+
+    <p>
+      If you have any questions, concerns, or requests regarding this Privacy Policy or the handling of your personal
+      information, please contact us at
+      <a href="mailto:support@pavrel.com" class="text-underline font-medium">support@pavrel.com</a>.
+    </p>
   </div>
 </template>
